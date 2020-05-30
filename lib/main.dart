83583649876a2c8654ac203263d7eca3ac321d49
TalhaTZA <talhavjtza@gmail.com>
2020-05-30 19:19:13 +0500

    ADD feature of adding word pair to favourites

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import './random_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: ThemeData(
           primarySwatch: Colors.purple,
         ),
-        home: RandomWords()
-    );
+        home: RandomWords());
   }
-}
-
-class RandomWords extends StatefulWidget {
-
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-
-}
-
-class _RandomWordsState extends State<RandomWords> {
-
-  final _randomWordPairs = <WordPair>[];
-
-  Widget _buildList() {
-    return ListView.builder(
-        padding: EdgeInsets.all(16.0),
-        itemBuilder: (context, item) {
-          if (item.isOdd) return Divider();
-
-          final _index = item ~/ 2;
-
-          if (_index >= _randomWordPairs.length) {
-            _randomWordPairs.addAll(generateWordPairs().take(10));
-          }
-
-          return ListTile(
-            title: Text(_randomWordPairs[_index].asPascalCase),
-          );
-        });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Word Pair Genarator'),
-      ),
-      body: _buildList(),
-    );
-  }
-
 }
---
+++ lib/random_words.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _randomWordPairs = <WordPair>[];
+  final _randomWordPairsSaved = Set<WordPair>();
+
+  Widget _buildList() {
+    return ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemBuilder: (context, item) {
+          if (item.isOdd) return Divider();
+
+          final _index = item ~/ 2;
+
+          if (_index >= _randomWordPairs.length) {
+            _randomWordPairs.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_randomWordPairs[_index]);
+        });
+  }
+
+  Widget _buildRow(WordPair randomWordPair) {
+    final _alreadySaved = _randomWordPairsSaved.contains(randomWordPair);
+
+    return ListTile(
+      title: Text(
+        randomWordPair.asPascalCase,
+        style: TextStyle(fontSize: 18.0),
+      ),
+      trailing: Icon(_alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: _alreadySaved ? Colors.red : null),
+      onTap: () {
+        setState(() {
+          if (_alreadySaved) {
+            _randomWordPairsSaved.remove(randomWordPair);
+          } else {
+            _randomWordPairsSaved.add(randomWordPair);
+          }
+        });
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Word Pair Genarator'),
+      ),
+      body: _buildList(),
+    );
+  }
+}
