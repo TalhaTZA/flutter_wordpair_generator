d9c8a1c40d50ee25bd5cc4fdf4787e8935bfe439
TalhaTZA <talhavjtza@gmail.com>
2020-05-30 18:57:29 +0500

    ADD custom widget and list for word pairs

--- lib/main.dart
+++ lib/main.dart
@@ -5,22 +5,54 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
 
-  final wordPair = WordPair.random();
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Word Pair Generator'),
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
         ),
-        body: Center(
-          child: Text(wordPair.asPascalCase),
-        ),
-      ),
+        home: RandomWords()
     );
   }
+}
+
+class RandomWords extends StatefulWidget {
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+
+}
+
+class _RandomWordsState extends State<RandomWords> {
+
+  final _randomWordPairs = <WordPair>[];
+
+  Widget _buildList() {
+    return ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemBuilder: (context, item) {
+          if (item.isOdd) return Divider();
+
+          final _index = item ~/ 2;
+
+          if (_index >= _randomWordPairs.length) {
+            _randomWordPairs.addAll(generateWordPairs().take(10));
+          }
+
+          return ListTile(
+            title: Text(_randomWordPairs[_index].asPascalCase),
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Word Pair Genarator'),
+      ),
+      body: _buildList(),
+    );
+  }
+
 }
